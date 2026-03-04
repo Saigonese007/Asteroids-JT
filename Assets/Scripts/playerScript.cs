36a28366e1ec41b08902140275025736c0d5b87b
Saigonese07 <musicjames08@gmail.com>
2026-03-04 08:42:44 +0100

    A1 + Video 1

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -1,11 +1,17 @@
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
 
 public class playerScript : MonoBehaviour
 {
     Rigidbody2D rb;
 
     public float thrust = 1;
-    public float rotationSpeed = 50;
+    public float rotationSpeed = 300;
+    public float maxSpeed = 15;
+
+    bool racecarPhysicsEnabled = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,5 +28,32 @@ public class playerScript : MonoBehaviour
         rb.AddRelativeForceY(-thrust * yAxis);
 
         rb.rotation -= xAxis * rotationSpeed * Time.deltaTime;
+
+        rb.angularVelocity = 0;
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            if (racecarPhysicsEnabled)
+            {
+                Debug.Log("race phys [OFF]");
+                racecarPhysicsEnabled = false;
+            }
+            else
+            {
+                Debug.Log("race phys [ON]");
+                racecarPhysicsEnabled = true;
+            }
+        }
+       
+        //racecar phys
+        if (racecarPhysicsEnabled)
+        {
+            rb.linearVelocity = Quaternion.EulerAngles(0, 0, rb.rotation) * Vector2.down * rb.linearVelocity.magnitude;
+        }
+
+        if (rb.linearVelocity.magnitude > maxSpeed)
+        {
+            rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+        }
     }
 }
